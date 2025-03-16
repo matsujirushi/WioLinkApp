1ab9585eb499fc0319cba8ec29b1f3a6d112aaa8
MATSUOKA Takashi <matsujirushi@live.jp>
2025-03-16 20:40:50 +0900

    1st

---
+++ WioLinkCLI/Program.cs
@@ -0,0 +1,178 @@
+﻿using CommandLine;
+using CommandLine.Text;
+using SeeedKK.WioLink;
+using System.Net.Sockets;
+using System.Runtime.InteropServices;
+using System.Text;
+
+class Program
+{
+    private const string NODE_IP = "192.168.4.1";
+    private const int NODE_PORT = 1025;
+
+    [Verb("prov", false, HelpText = "デバイスをプロビジョニングします。")]
+    class ProvOptions
+    {
+        [Option('s', "server", Required = true, HelpText = "接続するWioサーバーです。")]
+        public string? Server { get; set; }
+
+        [Option('u', "user", Required = true, HelpText = "Wioサーバーにログインするユーザー名です。")]
+        public string? User { get; set; }
+
+        [Option('p', "password", Required = false, HelpText = "Wioサーバーにログインするパスワードです。")]
+        public string? Password { get; set; }
+
+        [Option('S', "wifi-ssid", Required = true, HelpText = "デバイスに設定するWi-FiのSSIDです。")]
+        public string? WifiSsid { get; set; }
+
+        [Option('P', "wifi-password", Required = false, HelpText = "デバイスに設定するWi-Fiのパスワードです。")]
+        public string? WifiPassword { get; set; }
+    }
+
+    private static async Task<int> RunProvisioning(ProvOptions opts)
+    {
+        var service = new WioLinkService($"https://{opts.Server}");
+
+        // ログイン
+        Console.Write("Wioサーバーにログイン... ");
+        var user = await service.UserLoginAsync(opts.User!, opts.Password!);
+        service.SetToken(user.token);
+        //user = null;
+        Console.WriteLine("成功。");
+
+        // ノードを作成
+        Console.Write("ノードを作成... ");
+        var newNode = await service.CreateNodeAsync(service.TemporaryNodeName, service.BoardWioNode1_0);
+        Console.WriteLine($"成功。 node_sn:{newNode.node_sn}");
+
+        // ノードのアクセスポイントに接続
+        Console.WriteLine("ノードのアクセスポイントに接続して、ENTERキーを押してください。");
+        Console.ReadLine();
+
+        // ノードを設定
+        while (true)
+        {
+            try
+            {
+                Console.Write("ノードのバージョンを確認... ");
+                var version = await GetNodeVersion();
+                Console.WriteLine($"成功。 version:{version}");
+                if (version < 1.2) Console.WriteLine("WARNING: ファームウェアがドメイン名前解決に対応していません。更新を強く推奨します。");
+
+                Console.Write("ノードを設定中... ");
+                await ConfigureNode(newNode.node_sn!, newNode.node_key!, opts.WifiSsid!, opts.WifiPassword!, opts.Server!);
+                Console.WriteLine("成功。");
+                break;
+            }
+            catch (OperationCanceledException)
+            {
+                Console.WriteLine("タイムアウト。");
+                if (!RetryConfirmation("再試行しますか？[Yn]")) throw new ApplicationException();
+            }
+        }
+
+        // インターネットのアクセスポイントに接続
+        Console.WriteLine("インターネットのアクセスポイントに接続して、ENTERキーを押してください。");
+        Console.ReadLine();
+
+        // ノードのオンラインを待機
+        Console.WriteLine("ノードのオンラインを待機...");
+        for (int i = 0; i < 60; i++)
+        {
+            var nodes2 = await service.ListAllNodesOfUserAsync();
+            var node = nodes2.nodes?.Where(n => n.node_sn == newNode.node_sn).First();
+            Console.Write($" {i} ");
+            if (node == null) Console.WriteLine("unknown");
+            else Console.WriteLine(node.online ? "online" : "offline");
+            if (node?.online ?? false) break;
+            await Task.Delay(1000);
+        }
+        Console.WriteLine("成功。");
+
+        // ノードの名前を変更
+        Console.Write("ノードの名前を変更... ");
+        await service.RenameNodeAsync(newNode.node_sn!, "hahaha");
+        Console.WriteLine("成功。");
+
+        return 0;
+    }
+
+    static async Task Main(string[] args)
+    {
+        await CommandLine.Parser.Default.ParseArguments<ProvOptions>(args)
+            .MapResult(
+            (ProvOptions opts) => RunProvisioning(opts),
+            errs => Task.FromResult(1)
+            );
+    }
+
+    private static async Task<double> GetNodeVersion()
+    {
+        using UdpClient udpClient = new UdpClient(NODE_PORT);
+        using var cts = new CancellationTokenSource();
+        cts.CancelAfter(4000);
+        var token = cts.Token;
+
+        var waitForOk = Task.Run(async () =>
+        {
+            while (true)
+            {
+                var result = await udpClient.ReceiveAsync(token);
+                string message = Encoding.UTF8.GetString(result.Buffer);
+
+                double version;
+                if (result.RemoteEndPoint.ToString() == $"{NODE_IP}:{NODE_PORT}" && double.TryParse(message, out version)) return version;
+            }
+        }, token);
+        await Task.Delay(1000);
+
+        byte[] sendBytes = Encoding.ASCII.GetBytes("VERSION");
+        udpClient.Send(sendBytes, sendBytes.Length, NODE_IP, NODE_PORT);
+
+        var version = await waitForOk;
+
+        udpClient.Close();
+
+        return version;
+    }
+
+    private static async Task ConfigureNode(string nodeSn, string nodeKey, string wifiSsid, string wifiPassword, string server)
+    {
+        using UdpClient udpClient = new UdpClient(NODE_PORT);
+        using var cts = new CancellationTokenSource();
+        cts.CancelAfter(4000);
+        var token = cts.Token;
+
+        var waitForOk = Task.Run(async () =>
+        {
+            while (true)
+            {
+                var result = await udpClient.ReceiveAsync(token);
+                string message = Encoding.UTF8.GetString(result.Buffer);
+
+                if (result.RemoteEndPoint.ToString() == $"{NODE_IP}:{NODE_PORT}" && message == "ok\r\n") return;
+            }
+        }, token);
+        await Task.Delay(1000);
+
+        byte[] sendBytes = Encoding.ASCII.GetBytes($"APCFG: {wifiSsid}\t{wifiPassword}\t{nodeKey}\t{nodeSn}\t{server}\t{server}\t");
+        udpClient.Send(sendBytes, sendBytes.Length, NODE_IP, NODE_PORT);
+
+        await waitForOk;
+
+        udpClient.Close();
+    }
+
+    private static bool RetryConfirmation(string message)
+    {
+        string? input;
+        while (true)
+        {
+            Console.Write(message);
+            input = Console.ReadLine();
+
+            if (input == "y" || input == "Y" || input == "") return true;
+            if (input == "n" || input == "N") return false;
+        }
+    }
+}
---
+++ WioLinkService/WioLinkService.cs
@@ -0,0 +1,172 @@
+﻿using System.Net.Http.Headers;
+using System.Text.Json;
+
+namespace SeeedKK.WioLink;
+
+public class WioLinkService
+{
+    public WioLinkService(string wioLinkServer)
+    {
+        _WioLinkServer = wioLinkServer;
+    }
+
+    public void SetToken(string? token)
+    {
+        _Client.DefaultRequestHeaders.Authorization = token != null ? new AuthenticationHeaderValue("token", token) : null;
+    }
+
+    #region User management APIs
+
+    public async Task<UserLoginResponse> UserLoginAsync(string username, string password)
+    {
+        var values = new Dictionary<string, string>
+            {
+                { "email", username },
+                { "password", password }
+            };
+
+        using var response = await _Client.PostAsync($"{_WioLinkServer}/v1/user/login", new FormUrlEncodedContent(values));
+        var responseString = await response.Content.ReadAsStringAsync();
+
+        var responseObject = JsonSerializer.Deserialize<UserLoginResponse>(responseString);
+        if (responseObject == null ||
+            responseObject.error != null ||
+            string.IsNullOrEmpty(responseObject.token) ||
+            string.IsNullOrEmpty(responseObject.user_id))
+        {
+            throw new ApplicationException(responseObject?.error);
+        }
+
+        return responseObject!;
+    }
+
+    public class UserLoginResponse : ResponseBase
+    {
+        public string? token { get; set; }
+        public string? user_id { get; set; }
+    }
+
+    #endregion
+
+    #region Nodes management APIs
+
+    public async Task<CreateNodeResponse> CreateNodeAsync(string name, string board)
+    {
+        var values = new Dictionary<string, string>
+            {
+                { "name", name },
+                { "board", board }
+            };
+
+        using var response = await _Client.PostAsync($"{_WioLinkServer}/v1/nodes/create", new FormUrlEncodedContent(values));
+        var responseString = await response.Content.ReadAsStringAsync();
+
+        var responseObject = JsonSerializer.Deserialize<CreateNodeResponse>(responseString);
+        if (responseObject == null ||
+            responseObject.error != null ||
+            string.IsNullOrEmpty(responseObject.node_key) ||
+            string.IsNullOrEmpty(responseObject.node_sn))
+        {
+            throw new ApplicationException(responseObject?.error);
+        }
+
+        return responseObject!;
+    }
+
+    public class CreateNodeResponse : ResponseBase
+    {
+        public string? node_key { get; set; }
+        public string? node_sn { get; set; }
+    }
+
+    public async Task<NodeListResponse> ListAllNodesOfUserAsync()
+    {
+        using var response = await _Client.GetAsync($"{_WioLinkServer}/v1/nodes/list");
+        var responseString = await response.Content.ReadAsStringAsync();
+
+        var responseObject = JsonSerializer.Deserialize<NodeListResponse>(responseString);
+        if (responseObject == null ||
+            responseObject.error != null ||
+            responseObject.nodes == null)
+        {
+            throw new ApplicationException(responseObject?.error);
+        }
+
+        return responseObject!;
+    }
+
+    public class Node
+    {
+        public string name { get; set; } = string.Empty;
+        public string node_key { get; set; } = string.Empty;
+        public string node_sn { get; set; } = string.Empty;
+        public string? dataxserver { get; set; }
+        public string board { get; set; } = string.Empty;
+        public bool online { get; set; }
+    }
+
+    public class NodeListResponse : ResponseBase
+    {
+        public List<Node>? nodes { get; set; }
+    }
+
+    public async Task RenameNodeAsync(string node_sn, string name)
+    {
+        var values = new Dictionary<string, string>
+            {
+                { "node_sn", node_sn },
+                { "name", name }
+            };
+
+        using var response = await _Client.PostAsync($"{_WioLinkServer}/v1/nodes/rename", new FormUrlEncodedContent(values));
+        var responseString = await response.Content.ReadAsStringAsync();
+
+        var responseObject = JsonSerializer.Deserialize<ResultResponse>(responseString);
+        if (responseObject == null ||
+            responseObject.error != null ||
+            responseObject.result != "ok")
+        {
+            throw new ApplicationException(responseObject?.error ?? responseObject?.result);
+        }
+    }
+
+    public async Task DeleteNodeAsync(string node_sn)
+    {
+        var values = new Dictionary<string, string>
+            {
+                { "node_sn", node_sn }
+            };
+
+        using var response = await _Client.PostAsync($"{_WioLinkServer}/v1/nodes/delete", new FormUrlEncodedContent(values));
+        var responseString = await response.Content.ReadAsStringAsync();
+
+        var responseObject = JsonSerializer.Deserialize<ResultResponse>(responseString);
+        if (responseObject == null ||
+            responseObject.error != null ||
+            responseObject.result != "ok")
+        {
+            throw new ApplicationException(responseObject?.error ?? responseObject?.result);
+        }
+    }
+
+    #endregion
+
+    public string BoardWioLink1_0 { get; } = "Wio Link v1.0";
+    public string BoardWioNode1_0 { get; } = "Wio Node v1.0";
+
+    public string TemporaryNodeName { get; } = "node000";
+
+    public class ResponseBase
+    {
+        public string? error { get; set; }
+    }
+
+    private class ResultResponse : ResponseBase
+    {
+        public string? result { get; set; }
+    }
+
+    private string _WioLinkServer = string.Empty;
+    private HttpClient _Client = new HttpClient();
+
+}
